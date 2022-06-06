4fe1ca436ae67f0378b2783c72be40cb293f5be3
KseniaOlshanskaya <Qwertybobo654*>
2022-06-06 23:12:16 +0700

    Added new methods on table creation

--- TableMaker.py
+++ TableMaker.py
@@ -28,4 +28,37 @@ class TableMaker:
                            "Экспорт, тыс. дол": list_export, "Импорт, тыс. дол": list_import})
         return df
 
+    @staticmethod
+    def get_growth_rate_table(export_previous, export_current, import_previous, import_current):
+        amount_previous = export_previous + import_previous
+        amount_current = export_current + import_current
+        saldo_previous = export_previous - import_previous
+        saldo_current = export_current - import_current
+        dict_rate = {"Экспорт":[export_previous, export_current],
+                     "Импорт": [import_previous, import_current],
+                     "Оборот": [amount_previous, amount_current],
+                     "Сальдо": [saldo_previous, saldo_current]}
+        df = pd.DataFrame({"Показатель": ["Темп роста, %", "Темп прироста, %"]})
+        for indicator in dict_rate.keys():
+            rate_of_increase = (dict_rate[indicator][1] / dict_rate[indicator][0])*100 # темп роста
+            accession_rate = rate_of_increase - 100 # темп прироста
+            df[indicator] = [rate_of_increase, accession_rate]
+        df = df.T
+        return df
+
+    @staticmethod
+    def get_parts_table(export_region, export_district, export_country,
+                        import_region, import_district, import_country):
+        export_part_in_district = export_region/export_district
+        export_part_in_country = export_region / export_country
+        import_part_in_district = import_region/import_district
+        import_part_in_country = import_region / import_country
+        df = pd.DataFrame({"Субъект": ["СФО", "РФ"],
+                           "Экспорт": [export_part_in_district, export_part_in_country],
+                           "Импорт": [import_part_in_district, import_part_in_country]})
+        return df
+
+
+
+
 
